b5098be28662971dce0813225e53b67eb0d7fb25
HJ0528 <64176435+HJ0528@users.noreply.github.com>
2020-07-20 14:28:43 +0900

    tradeDao수정

--- Semiproject/src/com/semi/dao/TradeDao.java
+++ Semiproject/src/com/semi/dao/TradeDao.java
@@ -1,6 +1,5 @@
 package com.semi.dao;
 
-import com.semi.dto.TradeDto;
 import static common.JDBCTemplate.*;
 
 import java.sql.Connection;
@@ -10,69 +9,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.semi.dto.TradeDto;
+
 public class TradeDao {
-	public TradeDto viewTrade(String id) {
-		Connection con = getConnection();
-		PreparedStatement pstm = null;
-		ResultSet rs = null;
-		TradeDto res = new TradeDto();
-		String sql = " SELECT CODE, STOCKNAME, HOLDING, STATUS FROM TRADE_BOARD WHERE ID=? ";
-		
-		try {
-			pstm = con.prepareStatement(sql);
-			pstm.setString(1, id);
-			System.out.println("03. query 준비 : " + sql);
-			
-			rs = pstm.executeQuery();
-			System.out.println("04. query 실행 및 리턴");
-			
-			while (rs.next()) {
-				res.setstockName(rs.getString(1));
-				res.setHolding(rs.getInt(2));
-				res.setStatus(rs.getString(3));
-			}
-		} catch (SQLException e) {
-			System.out.println("3단계 / 4단계 오류");
-			e.printStackTrace();
-		} finally {
-			close(rs);
-			close(pstm);
-			close(con);
-		}
-		
-		
-		return res;
-	}
-	
-	public int countTrade(String id) {
-		Connection con = getConnection();
-		PreparedStatement pstm = null;
-		ResultSet rs = null;
-		int count = 0;
-		String sql = " SELECT COUNT(*) FROM TRADE_BOARD WHERE ID=? ";
-		
-		try {
-			pstm = con.prepareStatement(sql);
-			pstm.setString(1, id);
-			System.out.println("03. query 준비 : " + sql);
-			
-			rs = pstm.executeQuery();
-			System.out.println("04. query 실행 및 리턴");
-			
-			while (rs.next()) {
-				count = rs.getInt(1);
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			close(rs);
-			close(pstm);
-			close(con);
-		}
-		
-		return count;
-	}
+
 	Connection con = getConnection();
 	PreparedStatement pstm = null;
 	
@@ -224,5 +164,6 @@ public class TradeDao {
 		
 		return code;
 	}
+	
 	
 }
